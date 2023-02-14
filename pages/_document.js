b06c99d578418a8aa9a90b39f7a7cb20e75a085b
Golde Tischler <golde.tischler@gmail.com>
2023-02-14 16:09:15 +0100

    Portfolio basic design

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <link href="https://unpkg.com/nes.css@latest/css/nes.min.css" rel="stylesheet" />
+      
       <Head />
       <body>
         <Main />
