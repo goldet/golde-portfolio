b79da8596800634615b26737ed7e1b6eb9afbfbd
Golde Tischler <golde.tischler@gmail.com>
2023-02-15 20:21:37 +0100

    fixed project info buttons and hover text

---
+++ components/ProjectButtons.jsx
@@ -0,0 +1,153 @@
+import * as React from "react";
+import { useState } from "react";
+import Diversity1Icon from "@mui/icons-material/Diversity1";
+import DesktopMacIcon from "@mui/icons-material/DesktopMac";
+import DirectionsIcon from "@mui/icons-material/Directions";
+import Link from "next/link";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import Popover from "@mui/material/Popover";
+import Typography from "@mui/material/Typography";
+
+const ProjectButtons = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorEl2, setAnchorEl2] = useState(null);
+  const [anchorEl3, setAnchorEl3] = useState(null);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverOpen2 = (event) => {
+    setAnchorEl2(event.currentTarget);
+  };
+
+  const handlePopoverOpen3 = (event) => {
+    setAnchorEl3(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handlePopoverClose2 = () => {
+    setAnchorEl2(null);
+  };
+
+  const handlePopoverClose3 = () => {
+    setAnchorEl3(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const open2 = Boolean(anchorEl2);
+  const open3 = Boolean(anchorEl3);
+
+  return (
+    <>
+      <Stack
+        direction="row"
+        spacing={1}
+        sx={{
+          position: "absolute",
+          bottom: 16,
+          right: 16,
+        }}
+      >
+        <Typography
+          aria-owns={open ? "mouse-over-popover" : undefined}
+          aria-haspopup="true"
+          onMouseEnter={handlePopoverOpen}
+          onMouseLeave={handlePopoverClose}
+        >
+          <IconButton aria-label="Collaborators">
+            <Diversity1Icon sx={{ bgcolor: "#DAB7F9" }} />
+          </IconButton>
+        </Typography>
+        <Popover
+          id="mouse-over-popover"
+          sx={{
+            pointerEvents: "none",
+          }}
+          open={open}
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          onClose={handlePopoverClose}
+          disableRestoreFocus
+        >
+          <Typography sx={{ p: 1 }}>Collaborators</Typography>
+        </Popover>
+
+        <Typography
+          aria-owns={open2 ? "mouse-over-popover" : undefined}
+          aria-haspopup="true"
+          onMouseEnter={handlePopoverOpen2}
+          onMouseLeave={handlePopoverClose2}
+        >
+          <IconButton aria-label="Tech Stack">
+            <DesktopMacIcon sx={{ bgcolor: "#369CEE" }} />
+          </IconButton>
+        </Typography>
+        <Popover
+          id="mouse-over-popover"
+          sx={{
+            pointerEvents: "none",
+          }}
+          open={open2}
+          anchorEl={anchorEl2}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          onClose={handlePopoverClose2}
+          disableRestoreFocus
+        >
+          <Typography sx={{ p: 1 }}>Tech Stack</Typography>
+        </Popover>
+
+        <Typography
+          aria-owns={open3 ? "mouse-over-popover" : undefined}
+          aria-haspopup="true"
+          onMouseEnter={handlePopoverOpen3}
+          onMouseLeave={handlePopoverClose3}
+        >
+          <IconButton aria-label="Go to the project">
+            <DirectionsIcon sx={{ bgcolor: "#92CC41" }} />
+          </IconButton>
+        </Typography>
+        <Popover
+          id="mouse-over-popover"
+          sx={{
+            pointerEvents: "none",
+          }}
+          open={open3}
+          anchorEl={anchorEl3}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          onClose={handlePopoverClose3}
+          disableRestoreFocus
+        >
+          <Typography sx={{ p: 1 }}>Go to project</Typography>
+        </Popover>
+      </Stack>
+    </>
+  );
+};
+
+export default ProjectButtons;
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,170 +1,29 @@
-import { Inter } from "@next/font/google";
-import { useState } from "react";
 import * as React from "react";
-import Box from "@mui/material/Box";
-import SpeedDial from "@mui/material/SpeedDial";
-import SpeedDialIcon from "@mui/material/SpeedDialIcon";
-import SpeedDialAction from "@mui/material/SpeedDialAction";
-import Diversity1Icon from "@mui/icons-material/Diversity1";
-import DesktopMacIcon from "@mui/icons-material/DesktopMac";
-import DirectionsIcon from "@mui/icons-material/Directions";
-
-
-function handleDialogOpen (e) {
-  console.log(e)
-} 
-const inter = Inter({ subsets: ["latin"] });
-const actions = [
-  { icon: <Diversity1Icon sx={{ bgcolor: "#DAB7F9" }} />, name: "Collaborators", action: handleDialogOpen(), operation: "collaborators" },
-  { icon: <DesktopMacIcon sx={{ bgcolor: "#369CEE" }} />, name: "Tech Stack",  action: handleDialogOpen(), operation: "techstack" },
-  { icon: <DirectionsIcon sx={{ bgcolor: "#92CC41" }} />, name: "Go to Project",  action:  handleDialogOpen(), operation: "go"  },
-];
+import ProjectButtons from "./ProjectButtons";
 
 const Projects = () => {
-  const [open, setOpen] = useState(false);
-  const [open2, setOpen2] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  const handleOpen2 = () => setOpen(true);
-  const handleClose2 = () => setOpen(false);
-
-
-  //handler function
- function handleTap (e,operation){
-  e.preventDefault();
-  if(operation=="collaborators"){
-    console.log("one")
-  }else if(operation=="techstack"){
-    console.log("two")
-  }else if(operation=="go"){
-    console.log("three")
-  }
-  
-};
-
   return (
     <>
-
-      
-   
       <div className="portfolioGrid">
         <div className="img nes-container is-centered is-rounded is-dark">
-          {" "}
-          {/* with-title */}
           <p className="portfolioTitle title">Zero-Waste</p>
           <p className="portfolioDescription">
             This app attempts to reduce food waste while building community.
           </p>
-          <Box className="box">
-            <SpeedDial
 
-              /*  className="speedDialBox" */
-              ariaLabel="SpeedDial"
-              className="SpeedDial"
-              sx={{
-                position: "absolute",
-                bottom: 16,
-                right: 16,
-                color: "#F3ABCB",
-              }}
-              icon={<SpeedDialIcon />}
-              onTap={handleTap}
-              onClose={handleClose}
-              onOpen={handleOpen}
-              open={open}
-              FabProps={{
-                sx: {
-                  color: "none",
-                  bgcolor: "#F3ABCB",
-                  "&:hover": {
-                    bgcolor: "#F3ABCB",
-                  },
-                },
-              }}
-            >
-              {actions.map((action) => (
-                <SpeedDialAction
-                  className="SpeedDial"
-                  sx={{ color: "primary" }}
-                  key={action.name}
-                  icon={action.icon}
-                  tooltipTitle={action.name}
-                  onClick={handleClose}
-                  onTap={(e) => {
-                    handleTap(e.action.operation)
-                  }}
-                  FabProps={{
-                    sx: {
-                      color: "none",
-                      bgcolor: "none",
-                      "&:hover": {
-                        bgcolor: "none",
-                      },
-                    },
-                  }}
-                />
-              ))}
-            </SpeedDial>
-          </Box>
+          <ProjectButtons />
         </div>
 
         <div className="img nes-container is-centered is-rounded is-dark">
           {" "}
-          {/* with-title */}
           <p className="portfolioTitle title">Ben Schroeder Poetry</p>
           <p className="portfolioDescription">
-           This web app was made to showcase the work of poet and essayist, Ben Shroeder.
-            
+            This web app was made to showcase the work of poet and essayist, Ben
+            Shroeder.
           </p>
-          <Box className="box">
-            <SpeedDial
-              /*  className="speedDialBox" */
-              ariaLabel="SpeedDial"
-              className="SpeedDial"
-              sx={{
-                position: "absolute",
-                bottom: 16,
-                right: 16,
-                color: "#F3ABCB",
-              }}
-              icon={<SpeedDialIcon />}
-              onClos2e={handleClose2}
-              onOpen2={handleOpen2}
-              open2={open2}
-              FabProps={{
-                sx: {
-                  color: "none",
-                  bgcolor: "#F3ABCB",
-                  "&:hover": {
-                    bgcolor: "#F3ABCB",
-                  },
-                },
-              }}
-            >
-              {actions.map((action) => (
-                <SpeedDialAction
-                  className="SpeedDial"
-                  sx={{ color: "primary" }}
-                  key={action.name}
-                  icon={action.icon}
-                  tooltipTitle={action.name}
-                  onClick={handleClose2}
-                  FabProps={{
-                    sx: {
-                      color: "none",
-                      bgcolor: "none",
-                      "&:hover": {
-                        bgcolor: "none",
-                      },
-                    },
-                  }}
-                />
-              ))}
-            </SpeedDial>
-          </Box>
+          <ProjectButtons />
         </div>
       </div>
-   
     </>
   );
 };
